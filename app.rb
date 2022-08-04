1d4cf3e6594280feee57e5dbcbf0a0f5f616c92f
luisc <luis@twentysidedstore.com>
2022-08-04 10:36:10 -0400

    add write chart method and chart output to app script

--- app.rb
+++ app.rb
@@ -3,6 +3,30 @@
 require_relative 'lib/lottery'
 require_relative 'lib/ball'
 
+def write_chart_file(output_file, lottery)
+  data = {
+    title: lottery.title,
+    start_date: lottery.start_date,
+
+    white_ball_mean: lottery.white_ball_mean.round(2),
+    white_ball_standard_deviation: lottery.white_ball_standard_deviation.round(2),
+
+    yellow_ball_mean: lottery.yellow_ball_mean.round(2),
+    yellow_ball_standard_deviation: lottery.yellow_ball_standard_deviation.round(2),
+
+    labels_white_balls: lottery.white_ball_counter.keys,
+    data_white_balls: lottery.white_ball_counter.values,
+
+    labels_yellow_ball: lottery.yellow_ball_counter.keys,
+    data_yellow_ball: lottery.yellow_ball_counter.values
+  }
+
+  file = File.open("template/chart.rhtml")
+  file_data = file.read
+  file.close
+  File.write("output/#{output_file}", file_data % data)
+end
+
 mega_millions = Lottery.new(
   title: "Mega Millions",
   white_ball: Ball.new(range: 1..70, quantity: 5),
@@ -11,6 +35,7 @@ mega_millions = Lottery.new(
   start_date: "2017-10-31"
 )
 mega_millions.analyze
+write_chart_file("mega_millions.html", mega_millions)
 
 powerball = Lottery.new(
   title: "Powerball",
@@ -20,6 +45,7 @@ powerball = Lottery.new(
   start_date: "2015-10-07"
 )
 powerball.analyze
+write_chart_file("powerball.html", powerball)
 
 yotta = Lottery.new(
   title: "Yotta",
@@ -29,6 +55,7 @@ yotta = Lottery.new(
   start_date: "2020-09-27"
 )
 yotta.analyze
+write_chart_file("yotta.html", yotta)
 
 puts "How many combinations for Powerball would you like?"
 quantity = gets.chomp.to_i
